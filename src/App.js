2a43d2dd043296963992e2a29af6dff1df250b27
sjlee <happybin2013@google.com>
2023-01-25 17:55:28 +0900

    화면 route 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+import About from "./pages/About/About";
 
 function App() {
   return (
     <div>
-      test
+      <Routes>
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
-    
   );
 }
 
