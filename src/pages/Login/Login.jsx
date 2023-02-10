5eaea333d6194da0f1999791c54b98918c3f3f93
LEESJ <happybin2022@gmail.com>
2023-02-10 22:48:00 +0900

    merge

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,65 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Login = () => {
-  return;
-  <div>로그인 페이지</div>;
+  const [values, setValues] = useState({
+    userId: "",
+    passWord: "",
+    status: "off",
+    nickName: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "http://localhost:8080/saveUserInfo",
+        JSON.stringify(values, null),
+        {
+          headers: { "Content-Type": `application/json` },
+        }
+      )
+      .then((Response) => {
+        console.log(Response.data);
+      });
+  };
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <h1>로그인 페이지</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="userId"
+          placeholder="id"
+          value={values.id}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="nickName"
+          placeholder="nickName"
+          value={values.nickName}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="passWord"
+          placeholder="password"
+          value={values.passWord}
+          onChange={handleChange}
+        />
+        <button type="submit">LOGIN</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
