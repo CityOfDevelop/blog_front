3fc9828f77c972cb2fc732156d2da07aa56f6a39
LEESJ <happybin2022@gmail.com>
2023-02-10 22:21:10 +0900

    merge

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,55 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
+
+
 const Login = () => {
-  return;
-  <div>로그인 페이지</div>;
+
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  }
+  const onPasswordHandler = (event) => {
+      setPassword(event.currentTarget.value);
+  }
+
+  const dispatch = useDispatch();
+
+  const onSubmitHandler = (event) => {
+      // 버튼만 누르면 리프레시 되는것을 막아줌
+      event.preventDefault();
+
+      console.log('Email', Email);
+      console.log('Password', Password);
+      
+      let body = {
+          email: Email,
+          password: Password,
+      }
+
+      dispatch(loginUser(body));
+  }
+
+  return
+  <div style={{ 
+    display: 'flex', justifyContent: 'center', alignItems: 'center', 
+    width: '100%', height: '100vh'
+    }}>
+    <form style={{ display: 'flex', flexDirection: 'column'}}
+        onSubmit={onSubmitHandler}
+    >
+        <label>Email</label>
+        <input type='email' value={Email} onChange={onEmailHandler}/>
+        <label>Password</label>
+        <input type='password' value={Password} onChange={onPasswordHandler}/>
+        <br />
+        <button formAction=''>
+            Login
+        </button>
+    </form>
+</div>
 };
 
 export default Login;
