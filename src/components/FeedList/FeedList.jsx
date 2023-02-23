4af0c3a44ca841a5c930b25012b73911e9df21a0
sjlee <happybin2013@google.com>
2023-02-23 22:47:23 +0900

    add board

--- src/components/FeedList/FeedList.jsx
+++ src/components/FeedList/FeedList.jsx
@@ -1,15 +1,21 @@
+import "./FeedList.css";
+import { Link } from "react-router-dom";
+
 const FeedList = (props) => {
-    const testData = props.testData;
-    const listItems = testData.map((item) =>
-        <li id="list{item}" className="" key={item}>
-            {item}
-        </li>
-    )
+  const testData = props.testData;
+  const listItems = testData.map((item) => (
+    <li id="list{item}" className="board_list_item" key={item}>
+      <Link id="" to="/board/">
+        {/* 여기에 쿼리스트링 하셔서 상세 */}
+        {item.title}
+        <br />
+        {item.cntn}
+      </Link>
+    </li>
+  ));
   return (
-    <div>
-        <ul>
-            {listItems}
-        </ul>
+    <div className="wrap_board">
+      <ul className="board_list">{listItems}</ul>
     </div>
   );
 };
--- src/pages/InfoBoard/InfoBoard.jsx
+++ src/pages/InfoBoard/InfoBoard.jsx
@@ -14,11 +14,39 @@ const InfoBoard = () => {
     setActive((active) => false);
   };
 
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   axios
+  //     .get("http://localhost:8080/login", {
+  //       params: values,
+  //     })
+  //     .then((Response) => {
+  //       console.log(Response);
+  //       if (Response.status == "200" && Response.data != "") {
+  //         navigate("/");
+  //       } else {
+  //         setValues({ userId: "", passWord: "" });
+  //         alert("아이디or비번 확인해주세요");
+  //       }
+  //     });
+  // };
+
+  const testData1 = [
+    {
+      title: "제목1",
+      cntn: "내용테스트내용테스트내용테스트내용테스트내용테스트",
+    },
+    {
+      title: "제목2",
+      cntn: "내용테스트내용테스트내용테스트내용테스트내용테스트",
+    },
+  ];
+
   return (
-    <div>
+    <div className="dc_wrap">
       <Header sideOpen={sideOpen} />
       <SideBar sideClose={sideClose} active={active} />
-      <FeedList testData={[1, 2, 3]} />
+      <FeedList testData={testData1} />
       {/* 나중에 리스트 데이터 생기면 조회한 리스트 데이터로 리스트 생성  */}
     </div>
   );
