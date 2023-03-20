810e951fbdfa3413ba8b9b4fd27e18ca64c26a0b
minyonz <als543413@gmail.com>
2023-03-20 23:55:37 +0900

    마이페이지 개발중

--- src/containers/DashBoard/DashBoard.jsx
+++ src/containers/DashBoard/DashBoard.jsx
@@ -2,11 +2,35 @@ import PieChart from "components/PieChart/PieChart";
 import GridCrtf from "components/GridCrtf/GridCrtf";
 import PieChartDetail from "components/PieChartDetail/PieChartDetail";
 import "./DashBoard.css";
+import { useEffect } from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const DashBoard = () => {
+
+  const [info, setInfo] = useState({});
+  console.log("ㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗ");
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/userBasInfo/mykim01`)
+      .then((Response) => {
+        if (Response.status == "200" && Response.data != "") {
+          console.log(Response.data);
+          setInfo(Response.data);
+        } else {
+          alert("게시물이 존재하지 않습니다.");
+        }
+      })
+      .catch((error) => console.log(error));
+      return () => {
+        console.log("오와리다!")
+      }
+  });
+
   return (
     <div className="dash_wrap">
-      <div className="dash_intro dash_bg">소개{/* 자격증 */}</div>
+      <div className="dash_intro dash_bg">소개{info.intrd}{/* 자격증 */}</div>
       <div className="dash_chart_wrap">
         <div className="dash_chart">
           <PieChart />
@@ -17,7 +41,7 @@ const DashBoard = () => {
       </div>
 
       <div className="dash_crtf close dash_bg">
-        <GridCrtf />
+        <GridCrtf info={info}/>
       </div>
     </div>
   );
--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,35 +1,41 @@
 import Header from "containers/Header/Header";
 import SideBar from "containers/SideBar/SideBar";
 import Beacon from "containers/Beacon/Beacon";
-import React, { useState } from "react";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
 import "./MyPage.css";
 import { useSelector } from "react-redux";
 import Calendar from "components/Calendar/Calendar";
 import FeedList from "components/FeedList/FeedList";
-import { useEffect } from "react";
-import axios from "axios";
 import PieChart from "components/PieChart/PieChart";
 import DashBoard from "containers/DashBoard/DashBoard";
+import { useEffect } from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const MyPage = () => {
   const { sidebarStatus } = useSelector((state) => state.sidebarStatus);
-  const [feeds, setfeeds] = useState([]);
+
+  const [info, setInfo] = useState({});
+  console.log("ㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗ");
 
   useEffect(() => {
     axios
-      .get("http://localhost:8080/feedlist")
+      .get(`http://localhost:8080/userBasInfo/mykim01`)
       .then((Response) => {
         if (Response.status == "200" && Response.data != "") {
-          setfeeds(Response.data);
-          console.log(feeds);
+          console.log(Response.data);
+          setInfo(Response.data);
         } else {
           alert("게시물이 존재하지 않습니다.");
         }
       })
       .catch((error) => console.log(error));
-  }, []);
+      return () => {
+        console.log("おわりだ!")
+      }
+  });
+ 
   return (
     <div className="dc_wrap">
       <Header name="마이페이지" />
@@ -46,7 +52,7 @@ const MyPage = () => {
             <DashBoard />
           </Tab>
           <Tab eventKey="profile" title="게시물">
-            <FeedList feeds={feeds} />
+          {/* <FeedList feeds={feeds} /> */}
           </Tab>
           <Tab eventKey="contact" title="스케줄">
             <Calendar />
